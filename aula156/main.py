1e6022fa365339b957b0b33f9f2441cdcc3ba4a3
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2023-10-09 15:31:43 -0300

    DocStrings

--- aula156/main.py
+++ aula156/main.py
@@ -1,8 +1,12 @@
-import uma_linha
+# import uma_linha
 
-# print(dir(uma_linha))
-# print(uma_linha.__doc__)
-# print(uma_linha.__file__)
-# print(uma_linha.__name__)
+# # print(dir(uma_linha))
+# # print(uma_linha.__doc__)
+# # print(uma_linha.__file__)
+# # print(uma_linha.__name__)
 
-help(uma_linha)
+# help(uma_linha)
+
+import varias_linhas
+
+help(varias_linhas)
---
+++ aula156/varias_linhas.py
@@ -0,0 +1,13 @@
+"""O que o seu módulo faz
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel nulla neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames \
+   ac turpis egestas. In vitae est pharetra tellus volutpat venenatis ut et tellus. Aliquam tempus purus non nunc lobortis euismod. Etiam sed sapien dictum, \
+     tincidunt dolor vitae, tempus felis. Sed augue magna, tincidunt vel vehicula vitae, aliquam sed tortor. Nullam eget metus purus. Donec suscipit id  \
+     ligula eu pellentesque. Aliquam facilisis placerat neque, ac ornare quam accumsan eget. Donec non nisl eros. Mauris vestibulum lacus elit, sit amet \
+       convallis nunc luctus semper. Donec vulputate tincidunt sem. Nulla malesuada vitae erat et molestie. Nam ac eleifend nunc. \
+
+Proin nec mi sodales, sodales neque vitae, scelerisque velit. Sed luctus felis sit amet augue dignissim dignissim. Proin cursus turpis vel ipsum iaculis  \
+ malesuada. Cras sit amet semper nulla. Fusce ut ullamcorper arcu. Phasellus laoreet posuere malesuada. Aliquam et tortor ac purus sagittis commodo  \
+ ullamcorper scelerisque nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed ultricies volutpat  \
+ metus, sit amet euismod libero lobortis vitae. Nam sollicitudin non sem vitae pretium. Curabitur mauris enim, eleifend id urna accumsan, rhoncus  \
+ eleifend nibh. 
+"""
